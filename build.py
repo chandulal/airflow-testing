8822def043330dbb8b724bab2ce25a724af5dd39
chandulal.kavar <cckavar@gmail.com>
2018-09-12 23:52:48 +0800

    Use pybuilder tool to run unit test

---
+++ build.py
@@ -0,0 +1,17 @@
+from pybuilder.core import init,use_plugin
+
+@init
+def initialize(project):
+    project.set_property("run_unit_tests_propagate_stdout", True)
+    project.set_property("run_unit_tests_propagate_stderr", True)
+    project.depends_on("apache-airflow", "==1.9.0")
+    project.depends_on("cryptography", "==2.2.1")
+    project.set_property('verbose', True)
+
+use_plugin("exec")
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin('python.install_dependencies')
+
+default_task = ['clean']
+
