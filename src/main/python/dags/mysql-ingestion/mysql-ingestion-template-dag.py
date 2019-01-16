0ff30ac0931b2ec3ef3a7007f16036bb50c4702e
chandulal.kavar <cckavar@gmail.com>
2019-01-16 12:25:01 +0800

    Update the templates

--- src/main/python/dags/mysql-ingestion/mysql-ingestion-template-dag.py
+++ src/main/python/dags/mysql-ingestion/mysql-ingestion-template-dag.py
@@ -27,21 +27,21 @@ for y in glob.glob('*.yaml'):
     dag = DAG(dag_id=dag_meta['dag_id'], start_date=dag_meta['start_date'])
 
     task_bag = {}
-    pattern = dag_meta['pattern']
+    tasks = dag_meta['tasks']
 
-    for e in pattern['extract']:
+    for e in tasks['extract']:
         task_id = 'extract{}'.format(e['source'])
         task = PythonOperator(task_id=task_id, python_callable=extract, op_kwargs=e, dag=dag)
         task_bag[task_id] = task
 
-    for t in pattern['transform']:
+    for t in tasks['transform']:
         task_id = 'transform{}'.format(t['transform'])
         task = PythonOperator(task_id=task_id, python_callable=transform, op_kwargs=t, dag=dag)
 
         task.set_upstream(task_bag['extract{}'.format(t['transform'])])
         task_bag[task_id] = task
 
-    for l in pattern['load']:
+    for l in tasks['load']:
         task_id = 'load{}'.format(l['sink'])
         task = PythonOperator(task_id=task_id, python_callable=load, op_kwargs=l, dag=dag)
 
