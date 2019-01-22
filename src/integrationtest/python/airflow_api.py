ae93982f2aab81985ded627555f5ceac063c896d
Chandu Kavar <cckavar@ymail.com>
2019-01-22 23:13:15 +0800

    Refactore airflow api methods

---
+++ src/integrationtest/python/airflow_api.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import subprocess
+
+
+class AirflowAPI:
+    def __init__(self):
+        self.minikube_ip = self.get_minikube_ip()
+
+    def get_minikube_ip(self):
+        process = subprocess.Popen(["minikube", "ip"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+        if process.stdout is not None:
+            minikube_ip = process.stdout.readline().strip()
+            if not minikube_ip:
+                raise Exception("Minikube is not running. Please, start minikube first.")
+            return minikube_ip
+        raise Exception("Minikube is not running. Please, start minikube first.")
+
+    def get_airflow_url(self):
+        return "http://%s:%s" % (self.minikube_ip, 31317)
+
+    def unpause_dag(self, dag_id):
+        return requests.get(
+            "%s/admin/rest_api/api?api=unpause&dag_id=%s" % (self.get_airflow_url(), dag_id))
+
+    def pause_dag(self, dag_id):
+        return requests.get(
+            "%s/admin/rest_api/api?api=pause&dag_id=%s" % (self.get_airflow_url(), dag_id))
+
+    def trigger_dag(self, dag_id, execution_date):
+        self.unpause_dag(dag_id)
+        triggered_response = requests.get(
+            "%s/admin/rest_api/api?api=trigger_dag&dag_id=%s&exec_date=%s" % (
+                self.get_airflow_url(), dag_id, execution_date))
+        if triggered_response.status_code != 200:
+            raise Exception("Please, wait for airflow web server to start.")
+
+    def dag_state(self, dag_id, execution_date):
+        return requests.get(
+            "%s/admin/rest_api/api?api=dag_state&dag_id=%s&execution_date=%s" % (
+                self.get_airflow_url(), dag_id, execution_date))
+
+    def is_dag_running(self, dag_id, execution_date):
+        response = self.dag_state(dag_id, execution_date)
+        json_response = json.loads(response.text)
+        print(json_response)
+        if "success" not in json_response['output']['stdout']:
+            return True
+        else:
+            self.pause_dag(dag_id)
+            return False
--- src/integrationtest/python/dags/hello_world_tests.py
+++ src/integrationtest/python/dags/hello_world_tests.py
@@ -2,21 +2,21 @@ import sys
 import unittest
 
 sys.path.append('../')
-from utils import *
+from airflow_api import AirflowAPI
 
 
 class TestHelloWorldDag(unittest.TestCase):
     """Integration test for Hello world DAG"""
 
     def setUp(self):
-        self.minikube_ip = get_minikube_ip()
+        self.airflow_api = AirflowAPI()
 
     def test_hello_world(self):
         """helloword dag should run successfully"""
         execution_date = "2019-01-11T12:00:00+00:00"
         dag_id = "hello_world"
-        trigger_dag(dag_id, execution_date)
+        self.airflow_api.trigger_dag(dag_id, execution_date)
         is_running = True
         while is_running:
-            is_running = is_dag_running(dag_id, execution_date)
+            is_running = self.airflow_api.is_dag_running(dag_id, execution_date)
         self.assertEqual(is_running, False)
